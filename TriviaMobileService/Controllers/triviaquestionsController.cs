6baabd7a272f7a2aeded666db27bfcb006d7e512
gaob <nightfir@mail.com>
2015-02-25 10:29:18 +0800

    Update triviaQCount.

--- TriviaMobileService/Controllers/triviaquestionsController.cs
+++ TriviaMobileService/Controllers/triviaquestionsController.cs
@@ -56,7 +56,7 @@ namespace TriviaMobileService.Controllers
         }
 
         // GET tables/TodoItem/48D68C86-6EA6-4C25-AA33-223FC9A27959
-        [Route("api/triviaquestions/{triviaQCount:int}")]
+        [Route("api/triviaquestions")]
         public HttpResponseMessage GetTodoItem(int triviaQCount)
         {
             try
